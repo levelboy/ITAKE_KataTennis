a2ed49e871dec297108fb033fe6cf2e3faee48b5
adi <levelboy@gmail.com>
2013-05-30 15:30:21 +0300

    initial commit

---
+++ ITAKE_KataTennis.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace ITAKE_KataTennis.Test
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ ITAKE_KataTennis/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITAKE_KataTennis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
